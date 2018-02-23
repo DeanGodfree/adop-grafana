333f269d2108e1faf6febaa38c650bc80f5d2a59
DeanGodfree <deangodfree@gmail.com>
2018-02-23 12:22:58 +0000

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "moving config files in place and tokenising"
+
+mv /etc/grafana/grafana.ini /usr/share/grafana/conf/grafana_original.ini
+mv /usr/share/grafana/conf/grafana.ini /etc/grafana/ 
+
+#echo "LDAP_HOST="${LDAP_HOST}
+envsubst < /usr/share/grafana/conf/ldap_template.toml > /etc/grafana/adop_ldap.toml
+
+echo "Restart Grafana"
+: "${GF_PATHS_CONFIG:=/etc/grafana/grafana.ini}"
+: "${GF_PATHS_DATA:=/var/lib/grafana}"
+: "${GF_PATHS_LOGS:=/var/log/grafana}"
+: "${GF_PATHS_PLUGINS:=/var/lib/grafana/plugins}"
+: "${GF_PATHS_PROVISIONING:=/etc/grafana/provisioning}"
+
+chown -R 1000:1000 /etc/grafana/
+
+service grafana-server restart
